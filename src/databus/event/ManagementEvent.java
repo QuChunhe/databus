649014ea55d0a308a3c58f08867d2a9e3c3bf5d6
quchunhe-desk <chunhe.qu@gmail.com>
2015-10-23 21:49:36 +0800

    backup 2015-10-23

---
+++ src/databus/event/ManagementEvent.java
@@ -0,0 +1,8 @@
+package databus.event;
+
+import databus.core.Event;
+
+public interface ManagementEvent extends Event{
+    public static enum Type {}
+
+}
---
+++ src/databus/network/SubscribingSever.java
@@ -0,0 +1,12 @@
+package databus.network;
+
+public class SubscribingSever {    
+    
+    public SubscribingSever(Client client) {
+
+        this.client = client;
+    }
+
+    private Client client;
+
+}
