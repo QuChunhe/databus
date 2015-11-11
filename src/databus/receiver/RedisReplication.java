76f63559661c0c74a6d6b71c8bf109888de5f548
quchunhe-desk <chunhe.qu@gmail.com>
2015-11-11 19:27:39 +0800

    modify the properties

---
+++ src/databus/receiver/RedisReplication.java
@@ -0,0 +1,16 @@
+package databus.receiver;
+
+
+import databus.core.Event;
+
+
+public class RedisReplication extends RedisReceiver{
+
+    @Override
+    public void receive(Event event) {
+        
+                
+    }
+
+    
+}
