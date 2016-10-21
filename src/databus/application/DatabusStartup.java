4316faea83107ae92060326788389d73da93aeeb
quchunhe-desk <chunhe.qu@gmail.com>
2016-10-21 19:47:17 +0800

    fix: gracefull shutdown log

--- src/databus/application/DatabusStartup.java
+++ src/databus/application/DatabusStartup.java
@@ -1,5 +1,7 @@
 package databus.application;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -8,6 +10,7 @@ import org.apache.commons.logging.LogFactory;
 import databus.core.Listener;
 import databus.core.Publisher;
 import databus.core.Subscriber;
+import org.apache.logging.log4j.LogManager;
 
 public class DatabusStartup extends Startup {
 
@@ -39,6 +42,23 @@ public class DatabusStartup extends Startup {
         
         log.info("Databus has finished!");
         log.info("******************************************************************************");
+        LogFactory.releaseAll();
+        try {
+            Class clazz = Class.forName("org.apache.logging.log4j.LogManager",
+                                        false,
+                                        Thread.currentThread().getContextClassLoader());
+            Method method = clazz.getMethod("shutdown", null);
+            if (Modifier.isStatic(method.getModifiers())) {
+                LogManager.shutdown();
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("log4j packet does't exist!");
+            e.printStackTrace(System.out);
+        } catch (NoSuchMethodException e) {
+            System.out.println("version of log4j is less than 2.6!");
+            e.printStackTrace(System.out);
+        }
+        System.out.println("******************************************************************************");
     }
     
     private static Log log = LogFactory.getLog(DatabusStartup.class);
