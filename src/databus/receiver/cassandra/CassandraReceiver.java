5421a1b1019b186ac2b0438dc4fb2f6af870b06f
quchunhe-desk <chunhe.qu@gmail.com>
2018-06-10 17:57:57 +0800

    refactor: abstract factory method

--- src/databus/receiver/cassandra/CassandraReceiver.java
+++ src/databus/receiver/cassandra/CassandraReceiver.java
@@ -1,7 +1,6 @@
 package databus.receiver.cassandra;
 
 import java.io.IOException;
-import java.util.Properties;
 
 import com.datastax.driver.core.*;
 
@@ -11,7 +10,6 @@ import org.apache.commons.logging.LogFactory;
 import databus.core.Event;
 import databus.core.Receiver;
 import databus.util.FutureChecker;
-import databus.util.Helper;
 
 /**
  * Created by Qu Chunhe on 2018-05-24.
@@ -40,93 +38,12 @@ public class CassandraReceiver implements Receiver {
         this.cassandraBean = cassandraBean;
     }
 
-    public void setConfigFile(String configFile) {
-        Properties properties = Helper.loadProperties(configFile);
-
-        String contactPointsValue = properties.getProperty("contactPoints");
-        if ((null==contactPointsValue) || contactPointsValue.length()==0) {
-            throw new NullPointerException("contactPoints must have value!");
-        }
-        String[] contactPoints = contactPointsValue.split(",");
-        for(int i=0; i<contactPoints.length; i++) {
-            contactPoints[i] = contactPoints[i].trim();
-        }
-
-        PoolingOptions poolingOptions = new PoolingOptions();
-
-        String localCoreConnectionsPerHost = properties.getProperty("localCoreConnectionsPerHost");
-        if (null != localCoreConnectionsPerHost) {
-            poolingOptions.setCoreConnectionsPerHost(HostDistance.LOCAL,
-                                                     parseInt(localCoreConnectionsPerHost));
-        }
-        String remoteCoreConnectionsPerHost = properties.getProperty("remoteCoreConnectionsPerHost");
-        if (null != remoteCoreConnectionsPerHost) {
-            poolingOptions.setCoreConnectionsPerHost(HostDistance.REMOTE,
-                                                     parseInt(remoteCoreConnectionsPerHost));
-        }
-
-        String localMaxConnectionsPerHost = properties.getProperty("localMaxConnectionsPerHost");
-        if (null != localMaxConnectionsPerHost) {
-            poolingOptions.setMaxConnectionsPerHost(HostDistance.LOCAL,
-                                                    parseInt(localMaxConnectionsPerHost));
-        }
-        String remoteMaxConnectionsPerHost = properties.getProperty("remoteMaxConnectionsPerHost");
-        if (null != remoteMaxConnectionsPerHost) {
-            poolingOptions.setMaxConnectionsPerHost(HostDistance.REMOTE,
-                                                    parseInt(remoteMaxConnectionsPerHost));
-        }
-
-        String localNewConnectionThreshold = properties.getProperty("localNewConnectionThreshold");
-        if (null != localNewConnectionThreshold) {
-            poolingOptions.setNewConnectionThreshold(HostDistance.LOCAL,
-                                                     parseInt(localNewConnectionThreshold));
-        }
-        String remoteNewConnectionThreshold = properties.getProperty("remoteNewConnectionThreshold");
-        if (null != remoteNewConnectionThreshold) {
-            poolingOptions.setNewConnectionThreshold(HostDistance.REMOTE,
-                                                     parseInt(remoteNewConnectionThreshold));
-        }
-
-        String localMaxRequestsPerConnection = properties.getProperty("localMaxRequestsPerConnection");
-        if (null != localMaxRequestsPerConnection) {
-            poolingOptions.setMaxRequestsPerConnection(HostDistance.LOCAL,
-                                                       parseInt(localMaxRequestsPerConnection));
-        }
-        String remoteMaxRequestsPerConnection = properties.getProperty("remoteMaxRequestsPerConnection");
-        if (null != remoteMaxRequestsPerConnection) {
-            poolingOptions.setMaxRequestsPerConnection(HostDistance.REMOTE,
-                                                       parseInt(remoteMaxRequestsPerConnection));
-        }
-
-        String idleTimeoutSeconds = properties.getProperty("idleTimeoutSeconds");
-        if (null != idleTimeoutSeconds) {
-            poolingOptions.setIdleTimeoutSeconds(parseInt(idleTimeoutSeconds));
-        }
-
-        String maxQueueSize = properties.getProperty("maxQueueSize");
-        if (null != maxQueueSize) {
-            poolingOptions.setMaxQueueSize(parseInt(maxQueueSize));
-        }
-
-        String heartbeatIntervalSeconds = properties.getProperty("heartbeatIntervalSeconds");
-        if (null != heartbeatIntervalSeconds) {
-            poolingOptions.setHeartbeatIntervalSeconds(parseInt(heartbeatIntervalSeconds));
-        }
-
-        cluster = Cluster.builder()
-                         .addContactPoints(contactPoints)
-                         .withPoolingOptions(poolingOptions)
-                         .build();
-    }
-
-
     public void setFutureChecker(FutureChecker futureChecker) {
         this.futureChecker = futureChecker;
     }
 
-
-    private int parseInt(String value) {
-        return Integer.parseUnsignedInt(value);
+    public void setCluster(Cluster cluster) {
+        this.cluster = cluster;
     }
 
     private final static Log log = LogFactory.getLog(CassandraReceiver.class);
---
+++ src/databus/util/CassandraClusterBuilder.java
@@ -0,0 +1,96 @@
+package databus.util;
+
+import java.util.Properties;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.HostDistance;
+import com.datastax.driver.core.PoolingOptions;
+
+/**
+ * Created by Qu Chunhe on 2018-06-10.
+ */
+public class CassandraClusterBuilder {
+
+    public static Cluster build(String configFile) {
+        Properties properties = Helper.loadProperties(configFile);
+
+        String contactPointsValue = properties.getProperty("contactPoints");
+        if ((null==contactPointsValue) || contactPointsValue.length()==0) {
+            throw new NullPointerException("contactPoints must have value!");
+        }
+        String[] contactPoints = contactPointsValue.split(",");
+        for(int i=0; i<contactPoints.length; i++) {
+            contactPoints[i] = contactPoints[i].trim();
+        }
+
+        PoolingOptions poolingOptions = new PoolingOptions();
+
+        String localCoreConnectionsPerHost = properties.getProperty("localCoreConnectionsPerHost");
+        if (null != localCoreConnectionsPerHost) {
+            poolingOptions.setCoreConnectionsPerHost(HostDistance.LOCAL,
+                                                     parseInt(localCoreConnectionsPerHost));
+        }
+        String remoteCoreConnectionsPerHost = properties.getProperty("remoteCoreConnectionsPerHost");
+        if (null != remoteCoreConnectionsPerHost) {
+            poolingOptions.setCoreConnectionsPerHost(HostDistance.REMOTE,
+                                                     parseInt(remoteCoreConnectionsPerHost));
+        }
+
+        String localMaxConnectionsPerHost = properties.getProperty("localMaxConnectionsPerHost");
+        if (null != localMaxConnectionsPerHost) {
+            poolingOptions.setMaxConnectionsPerHost(HostDistance.LOCAL,
+                                                    parseInt(localMaxConnectionsPerHost));
+        }
+        String remoteMaxConnectionsPerHost = properties.getProperty("remoteMaxConnectionsPerHost");
+        if (null != remoteMaxConnectionsPerHost) {
+            poolingOptions.setMaxConnectionsPerHost(HostDistance.REMOTE,
+                                                    parseInt(remoteMaxConnectionsPerHost));
+        }
+
+        String localNewConnectionThreshold = properties.getProperty("localNewConnectionThreshold");
+        if (null != localNewConnectionThreshold) {
+            poolingOptions.setNewConnectionThreshold(HostDistance.LOCAL,
+                                                     parseInt(localNewConnectionThreshold));
+        }
+        String remoteNewConnectionThreshold = properties.getProperty("remoteNewConnectionThreshold");
+        if (null != remoteNewConnectionThreshold) {
+            poolingOptions.setNewConnectionThreshold(HostDistance.REMOTE,
+                                                     parseInt(remoteNewConnectionThreshold));
+        }
+
+        String localMaxRequestsPerConnection = properties.getProperty("localMaxRequestsPerConnection");
+        if (null != localMaxRequestsPerConnection) {
+            poolingOptions.setMaxRequestsPerConnection(HostDistance.LOCAL,
+                                                       parseInt(localMaxRequestsPerConnection));
+        }
+        String remoteMaxRequestsPerConnection = properties.getProperty("remoteMaxRequestsPerConnection");
+        if (null != remoteMaxRequestsPerConnection) {
+            poolingOptions.setMaxRequestsPerConnection(HostDistance.REMOTE,
+                                                       parseInt(remoteMaxRequestsPerConnection));
+        }
+
+        String idleTimeoutSeconds = properties.getProperty("idleTimeoutSeconds");
+        if (null != idleTimeoutSeconds) {
+            poolingOptions.setIdleTimeoutSeconds(parseInt(idleTimeoutSeconds));
+        }
+
+        String maxQueueSize = properties.getProperty("maxQueueSize");
+        if (null != maxQueueSize) {
+            poolingOptions.setMaxQueueSize(parseInt(maxQueueSize));
+        }
+
+        String heartbeatIntervalSeconds = properties.getProperty("heartbeatIntervalSeconds");
+        if (null != heartbeatIntervalSeconds) {
+            poolingOptions.setHeartbeatIntervalSeconds(parseInt(heartbeatIntervalSeconds));
+        }
+
+        return Cluster.builder()
+                      .addContactPoints(contactPoints)
+                      .withPoolingOptions(poolingOptions)
+                      .build();
+    }
+
+    private static int parseInt(String value) {
+        return Integer.parseUnsignedInt(value);
+    }
+}
