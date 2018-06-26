e0bfa4a0e3072dd9ea1127796d8c7a7fa037386f
quchunhe-desk <chunhe.qu@gmail.com>
2018-06-26 12:35:41 +0800

    fix: fetchSize problem

--- src/databus/application/Mysql2Cassandra.java
+++ src/databus/application/Mysql2Cassandra.java
@@ -65,68 +65,69 @@ public abstract class Mysql2Cassandra {
         try (Session session = cassandraCluster.connect();
              Connection conn = mysqlDataSource.getConnection();
              Statement stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY,
-                                                   ResultSet.CONCUR_READ_ONLY);
-             ResultSet rs = stmt.executeQuery(sql)) {
-            rs.setFetchSize(fetchSize);
-            ResultSetMetaData metaData = rs.getMetaData();
+                                                   ResultSet.CONCUR_READ_ONLY)) {
+            stmt.setFetchSize(fetchSize);
+            try (ResultSet rs = stmt.executeQuery(sql)) {
+                rs.setFetchSize(fetchSize);
+                ResultSetMetaData metaData = rs.getMetaData();
 
-            int columnCount = metaData.getColumnCount();
-            int[] types = new int[columnCount + 1];
-            for (int i = 1; i <= columnCount; i++) {
-                types[i] = metaData.getColumnType(i);
-            }
-            String[] columnNames = new String[columnCount + 1];
-            for (int i = 1; i <= columnCount; i++) {
-                columnNames[i] = metaData.getColumnName(i);
-            }
-            String CQL = toCQL(columnNames);
-            while (rs.next()) {
-                StringBuilder cql = new StringBuilder(CQL.length());
-                cql.append(CQL);
+                int columnCount = metaData.getColumnCount();
+                int[] types = new int[columnCount + 1];
                 for (int i = 1; i <= columnCount; i++) {
-                    if (1 == i) {
-                        cql.append("(");
-                    } else {
-                        cql.append(", ");
-                    }
-                    String value = rs.getString(i);
-                    if (null == value) {
-                        cql.append("null");
-                    } else if (Helper.doesUseQuotation(types[i])) {
-                        cql.append("'")
-                           .append(QUOTE_PATTERN.matcher(value).replaceAll("''"))
-                           .append("'");
-                    } else {
-                        cql.append(value);
-                    }
+                    types[i] = metaData.getColumnType(i);
                 }
-                cql.append(")");
-                counter.addTotalCount(1);
-                if (null == futureChecker) {
-                    try {
-                        session.execute(cql.toString());
-                        counter.addSuccessCount(1);
-                    } catch (Exception e) {
-                        log.error("Can not inset a row : "+cql.toString(), e);
-                        counter.addFailureCount(1);
+                String[] columnNames = new String[columnCount + 1];
+                for (int i = 1; i <= columnCount; i++) {
+                    columnNames[i] = metaData.getColumnName(i);
+                }
+                String CQL = toCQL(columnNames);
+                while (rs.next()) {
+                    StringBuilder cql = new StringBuilder(CQL.length());
+                    cql.append(CQL);
+                    for (int i = 1; i <= columnCount; i++) {
+                        if (1 == i) {
+                            cql.append("(");
+                        } else {
+                            cql.append(", ");
+                        }
+                        String value = rs.getString(i);
+                        if (null == value) {
+                            cql.append("null");
+                        } else if (Helper.doesUseQuotation(types[i])) {
+                            cql.append("'")
+                                    .append(QUOTE_PATTERN.matcher(value).replaceAll("''"))
+                                    .append("'");
+                        } else {
+                            cql.append(value);
+                        }
                     }
-                } else {
-                    ResultSetFuture future = session.executeAsync(cql.toString());
-                    futureChecker.check(future, new Callback<com.datastax.driver.core.ResultSet>() {
-                        @Override
-                        public void onFailure(Throwable t) {
-                            log.error("Can not inset a row : "+cql.toString(), t);
+                    cql.append(")");
+                    counter.addTotalCount(1);
+                    if (null == futureChecker) {
+                        try {
+                            session.execute(cql.toString());
+                            counter.addSuccessCount(1);
+                        } catch (Exception e) {
+                            log.error("Can not inset a row : " + cql.toString(), e);
                             counter.addFailureCount(1);
                         }
+                    } else {
+                        ResultSetFuture future = session.executeAsync(cql.toString());
+                        futureChecker.check(future, new Callback<com.datastax.driver.core.ResultSet>() {
+                            @Override
+                            public void onFailure(Throwable t) {
+                                log.error("Can not inset a row : " + cql.toString(), t);
+                                counter.addFailureCount(1);
+                            }
 
-                        @Override
-                        public void onSuccess(com.datastax.driver.core.ResultSet rows) {
-                            counter.addSuccessCount(1);
-                        }
-                    });
+                            @Override
+                            public void onSuccess(com.datastax.driver.core.ResultSet rows) {
+                                counter.addSuccessCount(1);
+                            }
+                        });
+                    }
                 }
             }
-
         } catch (SQLException e) {
             log.error("Select MySQL Error", e);
         }
